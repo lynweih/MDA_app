324e42ca97fa7374140c28ead6f1ab1956f50758
lynweih <161331329+lynweih@users.noreply.github.com>
2024-08-20 23:02:10 +0200

    Update Page_2.py

--- pages/Page_2.py
+++ pages/Page_2.py
@@ -16,7 +16,7 @@ from geopy.distance import geodesic
 cardiac = pd.read_csv(r"App Datasets/cardiac.csv",index_col=False)
 
 # Your Mapbox access token
-mapbox_access_token = 'pk.eyJ1Ijoid2VuaGFuY3UiLCJhIjoiY2x3cTlrYjlnMDAybTJqczdzOWFwcWpjdyJ9.yNEGbGM7vNhqbdfdfFNTng'
+mapbox_access_token = ''
 
 labels={"cluster":"Cardiac Arrest Level"}
 # Create the map
